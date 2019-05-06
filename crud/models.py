5e582c4e2841f760203a356eea5d1e6be0557295
SergioAra <47898195+SergioAra@users.noreply.github.com>
2019-05-06 15:39:00 -0500

    update query

--- crud/models.py
+++ crud/models.py
@@ -18,13 +18,15 @@ def metodoquery():
 
 def verViajes():
     query = """
-        SELECT Viaje.idViaje AS Codigo, Ruta.nombre AS Ruta, Empleado.nombre AS Conductor, Vehiculo.placa, 
-        Viaje.descripcion
+        SELECT Viaje.idViaje AS Codigo, Ruta.nombre AS Ruta, Empleado.nombre AS Conductor, Vehiculo.placa, Carga.nombre
+       
         FROM Viaje INNER JOIN Ruta ON Viaje.idRuta = Ruta.idRuta
         INNER JOIN viaje_vehiculo ON viaje_vehiculo.idViaje = Viaje.idViaje
         INNER JOIN Vehiculo ON Vehiculo.idVehiculo = viaje_vehiculo.idVehiculo
         INNER JOIN Vehiculo_empleado ON Vehiculo.idVehiculo = Vehiculo_empleado.idVehiculo
         INNER JOIN Empleado ON Empleado.idEmpleado = Vehiculo_empleado.idEmpleado
+        INNER JOIN Carga_viaje on Carga_viaje.idViaje = Viaje.idViaje
+        INNER JOIN Carga on Carga.idCarga = Carga_viaje.idCarga
     """
     with connection.cursor() as cursor:
         cursor.execute(query)
