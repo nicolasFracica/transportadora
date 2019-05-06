b6f01b65e5f2848eaed0f5f6415367f3e513929f
nicolas <nicolas.fracica@gmail.com>
2019-05-06 15:28:03 -0500

    More querys for tables

--- crud/models.py
+++ crud/models.py
@@ -1,4 +1,7 @@
 from django.db import connection
+import numpy as np
+import pandas as pd
+import matplotlib as plt
 
 
 def verViajes():
@@ -10,6 +13,35 @@ def verViajes():
         INNER JOIN Vehiculo ON Vehiculo.idVehiculo = viaje_vehiculo.idVehiculo
         INNER JOIN Vehiculo_empleado ON Vehiculo.idVehiculo = Vehiculo_empleado.idVehiculo
         INNER JOIN Empleado ON Empleado.idEmpleado = Vehiculo_empleado.idEmpleado
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        row = cursor.fetchall()
+
+    return row
+
+
+def verEmpleados():
+    query = """
+        SELECT Empleado.idEmpleado, Empleado.nombre, Empleado.documento, Cargo.nombre, Empleado.fechaNacimiento
+        FROM Empleado INNER JOIN Cargo ON Empleado.idCargo = Cargo.idCargo;
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        row = cursor.fetchall()
+
+    return row
+
+
+def verVehiculos():
+    query = """
+        SELECT Vehiculo.idVehiculo, Vehiculo.modelo, Vehiculo.placa, Tipo_de_mantenimiento.nombre
+        FROM Vehiculo INNER JOIN Vehiculo_mantenimiento 
+        ON Vehiculo_mantenimiento.id_vehiculo = Vehiculo.idVehiculo INNER JOIN Tipo_mantenimiento
+        ON Vehiculo_mantenimiento.id_mantenimiento = Tipo_mantenimiento.id_mantenimiento
+        INNER JOIN Tipo_de_mantenimiento 
+        ON Tipo_de_mantenimiento.idTipoDeMantenimiento = Tipo_mantenimiento.id_tipo
+        ORDER BY Vehiculo.idVehiculo ASC;
     """
     with connection.cursor() as cursor:
         cursor.execute(query)
--- database/querys.sql
+++ database/querys.sql
@@ -6,3 +6,20 @@ INNER JOIN viaje_vehiculo ON viaje_vehiculo.idViaje = Viaje.idViaje
 INNER JOIN Vehiculo ON Vehiculo.idVehiculo = viaje_vehiculo.idVehiculo
 INNER JOIN Vehiculo_empleado ON Vehiculo.idVehiculo = Vehiculo_empleado.idVehiculo
 INNER JOIN Empleado ON Empleado.idEmpleado = Vehiculo_empleado.idEmpleado;
+
+
+--Ver todos los empleados
+
+SELECT Empleado.idEmpleado, Empleado.nombre, Empleado.documento, Cargo.nombre, Empleado.fechaNacimiento
+FROM Empleado INNER JOIN Cargo ON Empleado.idCargo = Cargo.idCargo;
+
+
+--Ver todos los vehículos
+
+SELECT Vehiculo.idVehiculo, Vehiculo.modelo, Vehiculo.placa, Tipo_de_mantenimiento.nombre
+FROM Vehiculo INNER JOIN Vehiculo_mantenimiento 
+ON Vehiculo_mantenimiento.id_vehiculo = Vehiculo.idVehiculo INNER JOIN Tipo_mantenimiento
+ON Vehiculo_mantenimiento.id_mantenimiento = Tipo_mantenimiento.id_mantenimiento
+INNER JOIN Tipo_de_mantenimiento 
+ON Tipo_de_mantenimiento.idTipoDeMantenimiento = Tipo_mantenimiento.id_tipo
+ORDER BY Vehiculo.idVehiculo ASC;
