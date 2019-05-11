a3f53aab24702dc43227490e351cd7d60c6b9302
nicolas <nicolas.fracica@gmail.com>
2019-05-11 10:06:10 -0500

    Added querys

--- crud/models.py
+++ crud/models.py
@@ -92,6 +92,17 @@ def getCiudades():
 
     return row
 
+def getVehiculos():
+    query = """
+        Select v.placa
+        from vehiculo v
+    """
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        row = cursor.fetchall()
+
+    return row
+
 
 def empleado(id):
     query = """
@@ -129,3 +140,162 @@ def viaje(id):
         row = cursor.fetchall()
 
     return row
+
+def graph1():
+    delimage(1)
+    
+    engine = sql.create_engine('mysql+mysqldb://root:Sergiop9@localhost:3306/Transportadora')
+    busqueda2 = pd.read_sql_query("select ruta.nombre ruta,sum(factura.valor) valor,concat(ruta.nombre,' - ',sum(factura.valor)) concat, sum(factura.valor)*100/(select sum(factura.valor) from factura) Porcentaje from factura inner join viaje_factura on viaje_factura.idFactura = factura.idFactura inner join viaje on viaje.idViaje = viaje_factura.idViaje inner join ruta on ruta.idRuta = viaje.idRuta inner join ( select p.nombre parada, r.nombre ruta from tipo_parada_parada_ruta as tppr inner join parada p on p.idParada = tppr.idParada inner join tipo_parada tp on tp.idTipoParada = tppr.idTipoParada  inner join ruta r on r.idRuta = tppr.idRuta where tp.nombre = 'Fin') ruta_inicio on ruta.nombre = ruta_inicio.ruta group by ruta.nombre",engine)
+
+    colors = ['seagreen','darkgreen','lawngreen','lime','springgreen','aquamarine','aqua','deepskyblue','lightseagreen','mediumaquamarine']
+    plt.pie (busqueda2.Porcentaje, colors= colors, labels =busqueda2.ruta,radius = 2 ,autopct='%1.2f%%',shadow = True, explode = [0,0,0,0.1,0,0,0,0,0,0])
+    plt.legend (labels =busqueda2.concat,loc="center left", bbox_to_anchor=(1.5, 1.2))
+
+    
+    plt.savefig(parentDir+"/static/images/graph1.png", bbox_inches='tight')
+    plt.figure()
+
+def graph2():
+    delimage(2)
+    engine = sql.create_engine('mysql+mysqldb://root:Sergiop9@localhost:3306/Transportadora')
+    b1=pd.read_sql_query('select year(vi.fecha) fecha, month(vi.fecha) mes, sum(f.valor) valor from Empleado e inner join Emp_ve_vi evv on evv.idEmpleado = e.idEmpleado  inner join Vehiculo v on v.idVehiculo = evv.idVehiculo  inner join Viaje vi on vi.idViaje = evv.idViaje  inner join viaje_factura vf on vf.idViaje = vi.idViaje  inner join factura f on f.idFactura = vf.idFactura group by year(vi.fecha),month(vi.fecha) having fecha = "2019" ;',engine)
+    b2=pd.read_sql_query('select year(vi.fecha) fecha, month(vi.fecha) mes, sum(f.valor) valor from Empleado e inner join Emp_ve_vi evv on evv.idEmpleado = e.idEmpleado  inner join Vehiculo v on v.idVehiculo = evv.idVehiculo  inner join Viaje vi on vi.idViaje = evv.idViaje  inner join viaje_factura vf on vf.idViaje = vi.idViaje  inner join factura f on f.idFactura = vf.idFactura group by year(vi.fecha),month(vi.fecha) having fecha = "2018" ;',engine)
+    b3=pd.read_sql_query('select year(vi.fecha) fecha, month(vi.fecha) mes, sum(f.valor) valor from Empleado e inner join Emp_ve_vi evv on evv.idEmpleado = e.idEmpleado  inner join Vehiculo v on v.idVehiculo = evv.idVehiculo  inner join Viaje vi on vi.idViaje = evv.idViaje  inner join viaje_factura vf on vf.idViaje = vi.idViaje  inner join factura f on f.idFactura = vf.idFactura group by year(vi.fecha),month(vi.fecha) having fecha = "2017" ;',engine)
+    plt.bar(b3.mes,b3.valor, label="2017",color = 'lime')
+    plt.bar(b2.mes,b2.valor,label ="2018", color='yellowgreen')
+    plt.bar(b1.mes,b1.valor, label ="2019",color = 'deepskyblue')
+    plt.xticks([i for i in range(14)])
+    plt.xlabel("Mes del año")
+    plt.ylabel ("Ganancia")
+    plt.legend()
+
+    
+    plt.savefig(parentDir+"/static/images/graph2.png", bbox_inches='tight')
+    plt.figure()
+
+def graph3():
+    delimage(3)
+    engine = sql.create_engine('mysql+mysqldb://root:Sergiop9@localhost:3306/Transportadora')
+    b=pd.read_sql_query("SELECT Viaje.descripcion descripcion,Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS Conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte material de obras' GROUP BY Vehiculo.modelo UNION SELECT Viaje.descripcion descripcion, Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte material perecedero' GROUP BY Vehiculo.modelo UNION SELECT Viaje.descripcion descripcion, Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte de carga delicada'GROUP BY Vehiculo.modelo;", engine)
+    plt.style.use('seaborn-darkgrid')
+
+
+    colors = ['lime', 'limegreen','springgreen', 'mediumspringgreen','mediumaquamarine', 'aquamarine','turquoise', 'cyan', 'darkturquoise']
+    b.plot(kind='bar',x='modelo',y='Conteo', color=colors)
+    plt.xlabel('Modelo',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.ylabel('# de Autos por Modelo',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.title('NÚMERO DE AUTOS POR MODELO',fontname="Arial", fontsize=18,fontstyle='italic', fontweight='bold')
+    plt.xticks(size = 'medium', color = 'black', rotation = 45)
+    plt.yticks(size = 'medium', color = 'black')
+    
+    plt.savefig(parentDir+"/static/images/graph3.png", bbox_inches='tight')
+    plt.figure()
+
+def graph4():
+    delimage(4)
+    engine = sql.create_engine('mysql+mysqldb://root:Sergiop9@localhost:3306/Transportadora')
+    b=pd.read_sql_query("SELECT Viaje.descripcion descripcion,Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS Conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte material de obras' GROUP BY Vehiculo.modelo UNION SELECT Viaje.descripcion descripcion, Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte material perecedero' GROUP BY Vehiculo.modelo UNION SELECT Viaje.descripcion descripcion, Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte de carga delicada'GROUP BY Vehiculo.modelo;", engine)
+    plt.style.use('seaborn-darkgrid')
+
+
+    colors = ['lime', 'limegreen','springgreen', 'mediumspringgreen','mediumaquamarine', 'aquamarine','turquoise', 'cyan', 'darkturquoise']
+    b.plot(kind='bar',x='modelo',y='Conteo', color=colors)
+    plt.xlabel('Modelo',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.ylabel('# de Autos por Modelo',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.title('NÚMERO DE AUTOS POR MODELO',fontname="Arial", fontsize=18,fontstyle='italic', fontweight='bold')
+    plt.xticks(size = 'medium', color = 'black', rotation = 45)
+    plt.yticks(size = 'medium', color = 'black')
+    
+
+
+    colors = [ 'cyan','turquoise', 'darkturquoise']
+    b.groupby('descripcion')['modelo'].nunique().plot(kind='bar', color=colors)
+    plt.xlabel('Descripción',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.ylabel('# de Autos por Descripción',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.title('NÚMERO DE AUTOS QUE TRANSPORTAN LOS TIPOS DE CARGA',fontname="Arial", fontsize=15,fontstyle='italic', fontweight='bold')
+    plt.xticks(size = 'small', color = 'black', rotation = 45)
+    plt.yticks(size = 'small', color = 'black')
+    
+    plt.savefig(parentDir+"/static/images/graph4.png", bbox_inches='tight')
+    plt.figure()
+
+
+def graph5():
+    delimage(5)
+    engine = sql.create_engine('mysql+mysqldb://root:Sergiop9@localhost:3306/Transportadora')
+    b=pd.read_sql_query("SELECT Viaje.descripcion descripcion,Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS Conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte material de obras' GROUP BY Vehiculo.modelo UNION SELECT Viaje.descripcion descripcion, Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte material perecedero' GROUP BY Vehiculo.modelo UNION SELECT Viaje.descripcion descripcion, Vehiculo.modelo modelo, COUNT(Vehiculo.modelo)AS conteo FROM Viaje INNER JOIN Emp_ve_vi ON Emp_ve_vi.idViaje = Viaje.idViaje INNER JOIN Vehiculo ON Vehiculo.idVehiculo = Emp_ve_vi.idVehiculo WHERE Viaje.descripcion = 'Transporte de carga delicada'GROUP BY Vehiculo.modelo;", engine)
+    plt.style.use('seaborn-darkgrid')
+
+
+    colors = ['lime', 'limegreen','springgreen', 'mediumspringgreen','mediumaquamarine', 'aquamarine','turquoise', 'cyan', 'darkturquoise']
+    b.plot(kind='bar',x='modelo',y='Conteo', color=colors)
+    plt.xlabel('Modelo',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.ylabel('# de Autos por Modelo',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.title('NÚMERO DE AUTOS POR MODELO',fontname="Arial", fontsize=18,fontstyle='italic', fontweight='bold')
+    plt.xticks(size = 'medium', color = 'black', rotation = 45)
+    plt.yticks(size = 'medium', color = 'black')
+    
+
+
+    colors = [ 'cyan','turquoise', 'darkturquoise']
+    b.groupby('descripcion')['modelo'].nunique().plot(kind='bar', color=colors)
+    plt.xlabel('Descripción',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.ylabel('# de Autos por Descripción',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.title('NÚMERO DE AUTOS QUE TRANSPORTAN LOS TIPOS DE CARGA',fontname="Arial", fontsize=15,fontstyle='italic', fontweight='bold')
+    plt.xticks(size = 'small', color = 'black', rotation = 45)
+    plt.yticks(size = 'small', color = 'black')
+
+
+
+    colors = ['darksalmon', 'peachpuff','palegreen', 'paleturquoise','pink', 'wheat','khaki', 'lightskyblue', 'lightyellow']
+    ax=b.groupby(['descripcion','modelo'])['Conteo'].size().unstack().plot(kind='bar',stacked=True, color=colors, label='Modelo')
+    plt.xlabel('Descripción',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.ylabel('# de Autos por Descripción',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.suptitle('NÚMERO DE AUTOS QUE TRANSPORTAN LOS TIPOS DE CARGA',fontname="Arial", fontsize=13,fontstyle='italic', fontweight='bold')
+    plt.title('(Especificación de los Modelos)',fontname="Arial", fontsize=11,fontstyle='italic', fontweight='bold')
+    plt.xticks(size = 'small', color = 'black', rotation = 45)
+    plt.yticks(size = 'small', color = 'black')
+    chartBox = ax.get_position()
+    ax.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.8, chartBox.height])
+    ax.legend(loc='upper center', bbox_to_anchor=(1.3, 0.8), shadow=True, ncol=1)
+
+    
+    plt.savefig(parentDir+"/static/images/graph5.png", bbox_inches='tight')
+    plt.figure()
+
+
+
+def graph6():
+    delimage(6)
+    engine = sql.create_engine('mysql+mysqldb://root:Sergiop9@localhost:3306/Transportadora')
+    a=pd.read_sql_query("SELECT tipoMante.nombre mantenimiento, v.placa, viaje.descripcion,viaje.duracion, f.valor FROM tipo_de_mantenimiento tipoMante INNER JOIN mantenimiento m ON m.idTipoMantenimiento = tipoMante.idTipoMantenimiento INNER JOIN vehiculo_mantenimiento vM ON m.idMantenimiento = vM.id_mantenimiento INNER JOIN vehiculo v ON vM.id_vehiculo = v.idVehiculo INNER JOIN Emp_ve_vi evv on evv.idVehiculo = v.idVehiculo INNER JOIN viaje viaje ON evv.idViaje = viaje.idViaje INNER JOIN viaje_factura vF ON viaje.idViaje = vF.idViaje INNER JOIN factura f ON vF.idFactura = f.idFactura INNER JOIN (SELECT F.idFactura AS idFactura, F.valor AS valorFacturado,viaje.duracion AS duracionFacturado, viaje.idViaje AS idViaje FROM viaje Viaje INNER JOIN viaje_factura vF ON Viaje.idViaje = vF.idViaje INNER JOIN factura F ON vF.idFactura = F.idFactura WHERE vF.idFactura = F.idFactura AND vF.idViaje = viaje.idViaje)Facturado ON vF.idViaje = Facturado.idFactura WHERE m.idTipoMantenimiento = 1 ORDER BY valor;",engine)
+    plt.style.use('seaborn-darkgrid')
+    colors=['springgreen', 'mediumspringgreen','mediumaquamarine', 'aquamarine','turquoise', 'cyan', 'darkturquoise']
+    ax=a.plot(kind='bar',x='valor',y='duracion', color=colors)
+
+    ax.text(5.5, 16.0, "JHF887", transform=ax.transData)
+    ax.text(1.4, 16.0, "LADF345", transform=ax.transData)
+    ax.text(-0.5, 15.0, "LOI850", transform=ax.transData)
+    ax.text(3.5, 14.0, "LLO765", transform=ax.transData)
+    ax.text(0.5, 12.0, "HNG432", transform=ax.transData)
+    ax.text(2.5, 6.0, "VCX964", transform=ax.transData)
+    ax.text(4.6, 5.0, "NIC496", transform=ax.transData)
+
+    plt.xlabel('Valor',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.ylabel('Duración del Viaje (Horas)',fontname="Arial", fontsize=15, fontweight='bold')
+    plt.xticks(size = 'medium', color = 'black', rotation = 45)
+    plt.yticks(size = 'medium', color = 'black')
+    plt.title("VALOR DE LOS VIAJES, CON SU RESPECTIVA DURACIÓN", fontname="Arial", fontsize=18,fontstyle='italic', fontweight='bold')
+
+
+    chartBox = ax.get_position()
+    ax.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.8, chartBox.height])
+    ax.legend(loc='upper center', bbox_to_anchor=(3, 0.8), shadow=True, ncol=1)
+        
+    plt.savefig(parentDir+"/static/images/graph6.png", bbox_inches='tight')
+    plt.figure()
+
+def delimage(img):
+    strFile = parentDir+"static/images/graph"+str(img)+".png"
+    if os.path.isfile(strFile):
+        os.remove(strFile)   # Opt.: os.system("rm "+strFile)
